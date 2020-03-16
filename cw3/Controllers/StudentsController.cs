72fc75a90fbf9fd2780bb881fe37326ffe3eb261
s18977 <s18977@pjwstk.edu.pl>
2020-03-16 13:58:06 +0100

    Api db

--- cw3/Controllers/StudentsController.cs
+++ cw3/Controllers/StudentsController.cs
@@ -2,15 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using cw3.DAL;
+using cw3.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace cw3.Controllers
 {
-    public class StudentsController : Controller
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
         {
-            return View();
+            _dbService = dbService;
         }
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        //[HttpPost]
+        //public IActionResult CreateStudent(Student student)
+        //{
+        //    student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+        //    return Ok(student);
+        //}
+
+        //[HttpPut]
+        //public IActionResult PutData(int id)
+        //{
+        //    return Ok("Aktualizacja zakonczona");
+        //}
+
+        //[HttpDelete]
+        //public IActionResult DeleteData(int id)
+        //{
+        //    return Ok("Usuwanie zakończone");
+        //}
+
     }
 }
